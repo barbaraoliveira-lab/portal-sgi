224c344b1952626490ef59cbaf5dfc32afd20322
barbaraoliveira-lab <barbara.oliveira@integrajca.com.br>
2026-07-09 17:09:34 -0300

    Create script.js

---
+++ css/js/js/js/script.js
@@ -0,0 +1,19 @@
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+
+    });
+
+}, {
+    threshold: 0.15
+});
+
+document.querySelectorAll(".card").forEach(card => {
+    observer.observe(card);
+});
